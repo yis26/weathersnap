700a179225ec7b10c37f2009fee2c6eb43795407
yis26 <yis@njit.edu>
2026-04-23 10:18:07 -0400

    Add User Mongoose schema

---
+++ Backend/User.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
